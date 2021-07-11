f841cf9a122df61c20ae45f45b6a09d8c62ca119
Phu Pham <phamhungphu97@gmail.com>
2021-07-11 14:41:43 +0100

    uibuild

--- controllers/users.js
+++ controllers/users.js
@@ -2,12 +2,14 @@ const bcrypt = require("bcryptjs")
 const usersRouter = require("express").Router()
 const User = require("../models/user")
 
-usersRouter.get("/", async (request, response) => {
-    const users = await User
-        .find({})
-
-    response.json(users)
-})
+if (process.env.NODE_ENV === "development") {
+    usersRouter.get("/", async (request, response) => {
+        const users = await User
+            .find({})
+    
+        response.json(users)
+    })
+}
 
 usersRouter.post("/", async (request, response) => {
     const body = request.body
